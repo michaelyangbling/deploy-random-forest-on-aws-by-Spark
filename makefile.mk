7d9eece17bd174ea451a82ce71f0352f6467f339
minemine <yang.zho@husku.neu.edu>
2018-04-21 17:05:29 -0400

    clean data

--- makefile.mk
+++ makefile.mk
@@ -1,26 +1,24 @@
 #seperate control
-#e.x, in order to build jar file, : make -f make.mk build
+#e.x, in order to debug locally, : make -f makefile.mk standalone
 #for sbt: sbt package
 #for maven: mvn clean package
 #for maven: mvn scala:run -DmainClass=TestScala
 spark="/Users/yzh/Desktop/cour/parallel/spark-2.3.0-bin-hadoop2.7"
 job.name=TestScala
 local.master=local[4]
-app.name=Wiki Page Rank
+app.name=brain
 jar.name=/Users/yzh/IdeaProjects/mlSpark/target/scala-2.11/spark_2.11-0.1.jar
-local.input=/Users/yzh/Desktop/njtest/input
-local.output=/Users/yzh/Desktop/njtest/output
-num.iter=10
-k=100
+pArgs="/Users/yzh/Desktop/cour/parallel/brain/files/sample1.csv copy.bz2" "/Users/yzh/Desktop/cour/parallel/brain/sample5.bz2" "/Users/yzh/Desktop/cour/parallel/brain/output2"
 
 awsJar=spark_2.11-0.1.jar
-aws.input=workFold/input
-aws.output=output_sbt
+aws.inputTrain=train
+aws.inputTest=test
+aws.output=outputbrain
 aws.bucket.name=michaelyangcs
 aws.release.label=emr-5.11.1
 aws.instance.type=m4.large
 aws.num.nodes=19
-aws.log.dir=log_sbt
+aws.log.dir=logBrain2
 
 awsoutput="s3://michaelyangcs/workFold/output10"
 localout="/Users/yzh/Desktop/cour/parallel/RankOutput"
@@ -31,7 +29,7 @@ build:
 
 .PHONY:standalone
 standalone:
-	cd ${spark}; bin/spark-submit --class ${job.name} --master ${local.master} --name "${app.name}" ${jar.name} ${local.input} ${local.output} ${num.iter} ${k}
+	cd ${spark}; bin/spark-submit --class ${job.name} --master ${local.master} --name "${app.name}" ${jar.name} ${pArgs}
 
 
 .PHONY:awsrun
@@ -41,7 +39,7 @@ awsrun:
 		--release-label ${aws.release.label} \
 		--instance-groups '[{"InstanceCount":${aws.num.nodes},"InstanceGroupType":"CORE","InstanceType":"${aws.instance.type}"},{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"${aws.instance.type}"}]' \
 	    --applications Name=Hadoop Name=Spark \
-		--steps Type=CUSTOM_JAR,Name="${app.name}",Jar="command-runner.jar",ActionOnFailure=TERMINATE_CLUSTER,Args=["spark-submit","--deploy-mode","cluster","--class","${job.name}","s3://${aws.bucket.name}/${awsJar}","s3://${aws.bucket.name}/${aws.input}","s3://${aws.bucket.name}/${aws.output}","${num.iter}","${k}"] \
+		--steps Type=CUSTOM_JAR,Name="${app.name}",Jar="command-runner.jar",ActionOnFailure=TERMINATE_CLUSTER,Args=["spark-submit","--deploy-mode","cluster","--class","${job.name}","s3://${aws.bucket.name}/${awsJar}","s3://${aws.bucket.name}/${aws.inputTrain}","s3://${aws.bucket.name}/${aws.inputTest}","s3://${aws.bucket.name}/${aws.output}","${num.iter}","${k}"] \
 		--log-uri s3://${aws.bucket.name}/${aws.log.dir} \
 		--use-default-roles \
 		--ec2-attributes SubnetId=subnet-520b7f0f \
--- src/main/scala/TestScala.scala
+++ src/main/scala/TestScala.scala
@@ -1,6 +1,7 @@
 //standalone test: sbt package
 //make -f makefile.mk standalone
-//head -50000 /Users/yzh/Desktop/cour/parallel/brain/L6_1_965381.csv >> sample2
+//head -50000 /Users/yzh/Desktop/cour/parallel/brain/L6_1_965381.csv >> sample
+//bzip2 -k /Users/yzh/Desktop/cour/parallel/brain/sample2.csv
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
@@ -8,29 +9,49 @@ import org.apache.spark.mllib.tree.RandomForest
 import org.apache.spark.mllib.tree.model.RandomForestModel
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.SparkSession
+
+import scala.util.Try
 //this method store graph and PageRanksas rdd,
 //optimized for memory but use a little more time
+//"/Users/yzh/Desktop/cour/parallel/brain/files/sample1.csv copy.bz2" "/Users/yzh/Desktop/cour/parallel/brain/sample5.bz2" "/Users/yzh/Desktop/cour/parallel/brain/output2"
 object TestScala {
 
   def main(args: Array[String]):Unit= {
-    val conf = new SparkConf().setMaster("local").setAppName("My App")  //for local run&debug
-    //val conf = new SparkConf().setAppName("My App") //for AWS run
+    //val conf = new SparkConf().setMaster("local").setAppName("My App")  //for local run&debug
+    val conf = new SparkConf().setAppName("My App") //for AWS run
     val sc = new SparkContext(conf)
-    val data = sc.textFile("/Users/yzh/Desktop/cour/parallel/brain/files")// Should be some file on your system
-    val splits = data.randomSplit(Array(0.7, 0.3))
-    val (trainingData, testData) = (splits(0), splits(1))
+    val train = sc.textFile(args(0))// Should be some file on your system
+    val test = sc.textFile(args(1))
+    //val splits = data.randomSplit(Array(0.7, 0.3))
+    //val (trainingData, testData) = (splits(0), splits(1))
 //    val TrainingData=trainingData.map{record=>
 //      val features=record.slice(0,record.size-2).map(_.toDouble)
 //      val label=record(record.size-1).toInt
 //      LabeledPoint(label,Vectors.dense(features))
 //    }
-    val TrainingData = trainingData.map { line =>
-      val parts = line.split(',').map(_.toDouble)
-      LabeledPoint(parts.last.toInt, Vectors.dense(parts.init))
+    def cleanData(line:String): Array[LabeledPoint] = {
+      try {
+        val parts = line.split(',').map(_.toDouble)
+        Array(LabeledPoint(parts.last.toInt, Vectors.dense(parts.init)))
+      }
+      catch{
+        case _: Throwable => {
+          Array()
+        }
+      }
     }
-    val TestingData = trainingData.map { line =>
+    def
+
+    val TrainingData = train.map { line =>
       val parts = line.split(',').map(_.toDouble)
-      LabeledPoint(parts.last.toInt, Vectors.dense(parts.init))
+      LabeledPoint(parts.last.toInt, Vectors.dense(parts.init)
+
+
+      )
+    }
+    val TestingData = test.map { line =>
+      val parts = line.split(',').init.map(_.toDouble)
+      LabeledPoint(1, Vectors.dense(parts))
     }
 
 
@@ -50,7 +71,7 @@ object TestScala {
       val prediction = model.predict(point.features)
       prediction.toInt
     }
-    Preds.coalesce(1).saveAsTextFile("target/output")
+    Preds.coalesce(1).saveAsTextFile(args(2))
 //    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / TestingData.count()
 //    println("Test Error = " + testErr)
 //    println("Learned classification forest model:\n" + model.toDebugString)
